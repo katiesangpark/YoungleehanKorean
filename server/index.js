494be5f21276ed85a23a09ef6ec93349271050dc
Younghyun Lee <younghyunlee22@gmail.com>
2023-07-24 10:59:16 -0500

    jsonwebtoken login middlewares

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors";
+import morgan from "morgan";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose
   .catch((err) => console.log("DB ERROR => ", err));
 
 // middlewares
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
